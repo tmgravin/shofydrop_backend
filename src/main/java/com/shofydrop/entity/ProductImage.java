5e894fc5784119a00dcc423b35b188c743017d5f
Kshitiz <kshitizbabik.37@gmail.com>
2024-07-01 18:46:38 +0545

    product images

---
+++ src/main/java/com/shofydrop/entity/ProductImage.java
@@ -0,0 +1,27 @@
+package com.shofydrop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="product_image")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+
+//    private String imageUrl;
+//    private Timestamp createdAt;
+//    private Timestamp updatedAt;
+
+}
