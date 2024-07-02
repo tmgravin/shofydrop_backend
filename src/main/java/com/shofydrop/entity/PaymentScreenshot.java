ebb3cef3b867c1a05a7d906f7565237c92c5e69f
Kshitiz <kshitizbabik.37@gmail.com>
2024-07-02 16:19:11 +0545

    User Images Section

---
+++ src/main/java/com/shofydrop/entity/PaymentScreenshot.java
@@ -0,0 +1,28 @@
+package com.shofydrop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "payment_screenshot")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentScreenshot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id")
+    private Long id;
+
+//    private Order order;
+    @Column(name = "image", columnDefinition = "TEXT")
+    private String image;
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp createdAt;
+    @Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Timestamp updatedAt;
+}
---
+++ src/main/java/com/shofydrop/repository/HeroSectionImageRepository.java
@@ -0,0 +1,7 @@
+package com.shofydrop.repository;
+
+import com.shofydrop.entity.HeroSectionImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeroSectionImageRepository extends JpaRepository<HeroSectionImage, Long> {
+}
