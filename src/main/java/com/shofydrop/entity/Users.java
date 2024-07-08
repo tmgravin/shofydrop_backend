a461e8a56872c42a1dff9c1dffa1dcfb4db5f5fd
Kshitiz <kshitizbabik.37@gmail.com>
2024-07-08 11:06:29 +0545

    PostgresSql Setup

--- src/main/java/com/shofydrop/entity/Users.java
+++ src/main/java/com/shofydrop/entity/Users.java
@@ -44,14 +44,4 @@ public class Users {
     @Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false, updatable = false)
     private Timestamp updatedAt;
 
-    @PrePersist
-    protected void onCreate() {
-        createdAt = new Timestamp(System.currentTimeMillis());
-        updatedAt = new Timestamp(System.currentTimeMillis());
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = new Timestamp(System.currentTimeMillis());
-    }
 }
