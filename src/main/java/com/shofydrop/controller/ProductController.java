44e52050420e0a265fde00602bdd0fd13a5a5399
RANAS7 <deepakrana.dr537@gmail.com>
2024-07-08 11:53:50 +0545

    kjdf

--- src/main/java/com/shofydrop/controller/ProductController.java
+++ src/main/java/com/shofydrop/controller/ProductController.java
@@ -14,7 +14,7 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ProductController.class);
+//    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ProductController.class);
 
     @GetMapping("/getAllProducts")
     public ResponseEntity<?> getAllProducts() {
@@ -23,26 +23,26 @@ public class ProductController {
 
     @GetMapping("/getProduct/{id}")
     public ResponseEntity<?> getProduct(@PathVariable("id") Long id) {
-        log.info("Getting Product with ID: " + id);
+//        log.info("Getting Product with ID: " + id);
         return ResponseEntity.status(HttpStatus.OK).body(productService.findById(id));
     }
 
     @PostMapping("/addProduct")
     public ResponseEntity<?> addProduct(@RequestBody Product product) {
-        log.info("Saving Product: " + product.toString());
+//        log.info("Saving Product: " + product.toString());
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(product));
     }
 
     @DeleteMapping("/deleteProduct/{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable("id") Long id) {
-        log.info("Deleting Product with ID: " + id);
+//        log.info("Deleting Product with ID: " + id);
         productService.delete(id);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PutMapping("/updateProduct/{id}")
     public ResponseEntity<?> updateProduct(@RequestBody Product product) {
-        log.info("Updating Product: " + product.toString());
+//        log.info("Updating Product: " + product.toString());
         return ResponseEntity.status(HttpStatus.OK).body(productService.update(product));
     }
 }
