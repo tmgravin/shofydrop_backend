19b9a20a8c0b99e0ecdc72abe5a0bf89b84aac24
Kshitiz <kshitizbabik.37@gmail.com>
2024-07-12 19:58:45 +0545

    Email link verification

--- src/main/java/com/shofydrop/utils/MailUtils.java
+++ src/main/java/com/shofydrop/utils/MailUtils.java
@@ -3,9 +3,14 @@ package com.shofydrop.utils;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 
 @Service
@@ -30,18 +35,23 @@ public class MailUtils {
         javaMailSender.send(mimeMessage);
 
     }
-//  Mail configuration for user verification code
-    public void emailVerificationCode(String toEmail, int verificationCode) throws MessagingException {
+//  Mail configuration for user verification token
+    public void emailVerificationCode(String toEmail, String verificationLink) throws MessagingException, IOException {
 
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
         String subject = "Email verification for Shofydrop login.";
-        String content = "Your Verification code for Shofydrop login is: "+ verificationCode;
+
+        //Load html templates
+        ClassPathResource htmlFile = new ClassPathResource("templates/verifyEmail.html");
+        String htmlContent = StreamUtils.copyToString(htmlFile.getInputStream(), StandardCharsets.UTF_8);
+
+        htmlContent = htmlContent.replace("[link]", verificationLink);
 
         helper.setTo(toEmail);
         helper.setSubject(subject);
-        helper.setText(content, true);
+        helper.setText(htmlContent, true);
 
         javaMailSender.send(mimeMessage);
 
