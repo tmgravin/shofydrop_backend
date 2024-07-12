c76d2d19c9356af7215d0f9413fcde5bc2487da2
Kshitiz <kshitizbabik.37@gmail.com>
2024-07-12 13:44:13 +0545

    Documentation

--- src/main/java/com/shofydrop/service/impl/UserServiceImpl.java
+++ src/main/java/com/shofydrop/service/impl/UserServiceImpl.java
@@ -90,23 +90,23 @@ public class UserServiceImpl implements UserService {
         }
     }
 
-    //Implementation for signing user
+    //Implementation for user signup
     @Override
     public Users signupUser(Users users) {
         try {
             users.setPassword(DigestUtils.md5DigestAsHex(users.getPassword().getBytes()));
 
             //Ensure default value are set
-            if(users.getKycCompleted() == '\0'){
+            if (users.getKycCompleted() == '\0') {
                 users.setKycCompleted('N');
             }
-            if(users.getIsVerified() == '\0'){
+            if (users.getIsVerified() == '\0') {
                 users.setIsVerified('N');
             }
-            if(users.getUserType() == null){
+            if (users.getUserType() == null) {
                 users.setUserType(UserType.USER);
             }
-            if(users.getLoginType() == null){
+            if (users.getLoginType() == null) {
                 users.setLoginType(LoginType.EMAIL);
             }
 
@@ -118,75 +118,76 @@ public class UserServiceImpl implements UserService {
         }
     }
 
-    //Implementation for verify User after signup
+    //Implementation for sending email verification code
     @Override
     public void sendVerificationEmail(String email) {
         try {
             Users user = usersRepository.findByEmail(email).orElseThrow(() ->
-                    new ResourceNotFoundException("User doesn't exist with this email: "+email));
+                    new ResourceNotFoundException("User doesn't exist with this email: " + email));
             int verificationCode = generateVerificationCode();
             session.setAttribute("verificationEmail", email);
             session.setAttribute("vCode", verificationCode);
             mailUtils.emailVerificationCode(email, verificationCode);
             log.info("Verification code send to: {}", email);
-        }catch (Exception e){
-            log.error("Error during sending verification email.",e);
-            throw new RuntimeException("Internal Server Error:"+e.getMessage());
+        } catch (Exception e) {
+            log.error("Error during sending verification email.", e);
+            throw new RuntimeException("Internal Server Error:" + e.getMessage());
         }
     }
 
+    //Implementation for verifying email with code
     @Override
-    public void verifyUserEmail(int verificationCode) {
-        try{
+    public void verifyEmailCode(int verificationCode) {
+        try {
             String storedEmail = (String) session.getAttribute("verificationEmail");
             Integer storedVerificationCode = (Integer) session.getAttribute("vCode");
-            if(storedEmail == null || storedVerificationCode == null){
+            if (storedEmail == null || storedVerificationCode == null) {
                 throw new IllegalStateException("Email or verification code not found in the session. User verification failed.");
             }
-            if(storedVerificationCode.equals(verificationCode)){
+            if (storedVerificationCode.equals(verificationCode)) {
                 Users user = usersRepository.findByEmail(storedEmail).orElseThrow(() ->
-                        new ResourceNotFoundException("User doesn't exist with this email: "+storedEmail));
+                        new ResourceNotFoundException("User doesn't exist with this email: " + storedEmail));
                 user.setIsVerified('Y');
                 user.setUpdatedAt(Timestamp.from(Instant.now()));
                 usersRepository.save(user);
                 log.info("User is verified: {}", storedEmail);
                 session.removeAttribute("vCode");
                 session.removeAttribute("verificationEmail");
-            }else{
+            } else {
                 throw new ResourceNotFoundException("Invalid Verification Code.");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Error during user verification.", e);
-            throw new RuntimeException("Internal server error: "+e.getMessage());
+            throw new RuntimeException("Internal server error: " + e.getMessage());
         }
     }
 
-    //Implementation for user login after verification
+    //Implementation for login user after verifying email
     @Override
     public Users loginUser(String email, String password) {
         try {
-            Users user = usersRepository.findByEmail(email).orElseThrow(()->
+            Users user = usersRepository.findByEmail(email).orElseThrow(() ->
                     new ResourceNotFoundException("Email and Password don't match!"));
-            if(user.getPassword().equals(DigestUtils.md5DigestAsHex(password.getBytes()))){
-                if (user.getIsVerified() =='N'){
+            if (user.getPassword().equals(DigestUtils.md5DigestAsHex(password.getBytes()))) {
+                if (user.getIsVerified() == 'N') {
                     throw new IllegalStateException("User is not verified, please verify your email");
                 }
                 return user;
-            }else {
+            } else {
                 throw new ResourceNotFoundException("Email and password don't match!!");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Error during login", e);
-            throw new RuntimeException("Internal server Error: "+e.getMessage());
+            throw new RuntimeException("Internal server Error: " + e.getMessage());
         }
     }
 
-    //Implementation for forgetPassword, VerifyUser and ResetPassword
+    //Implementation for sending password reset code
     @Override
     public void forgetPassword(String email) {
-        try{
+        try {
             Users user = usersRepository.findByEmail(email).orElseThrow(() ->
-                    new ResourceNotFoundException("User doesn't exist with this email: "+email));
+                    new ResourceNotFoundException("User doesn't exist with this email: " + email));
             int verificationCode = generateVerificationCode();
             session.setAttribute("resetPasswordVerificationCode", verificationCode);
             session.setAttribute("resetPasswordEmail", email);
@@ -194,56 +195,59 @@ public class UserServiceImpl implements UserService {
             // Send verification code to user's email
             mailUtils.forgetPasswordVerificationCode(email, verificationCode);
             log.info("Verification code send to a: {}", email);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Error during forget password.", e);
-            throw new RuntimeException("Internal server Error: "+e.getMessage());
+            throw new RuntimeException("Internal server Error: " + e.getMessage());
         }
     }
 
+    //Implementation for verifying password reset code
     @Override
-    public void verifyCode(int verificationCode) {
+    public void verifyPasswordResetCode(int verificationCode) {
         try {
             String storedEmail = (String) session.getAttribute("resetPasswordEmail");
             Integer storedVerificationCode = (Integer) session.getAttribute("resetPasswordVerificationCode");
 
-            if(storedEmail == null || storedVerificationCode == null){
+            if (storedEmail == null || storedVerificationCode == null) {
                 throw new IllegalStateException("Email or verification code not found in the session. User verification failed.");
             }
             if (storedVerificationCode == verificationCode) {
                 log.info("Code verified successfully.");
-            }else{
+            } else {
                 throw new ResourceNotFoundException("Invalid Verification Code.");
             }
         } catch (Exception e) {
             log.error("Error during user verification.");
-            throw new RuntimeException("Internal server error: "+e.getMessage());
+            throw new RuntimeException("Internal server error: " + e.getMessage());
         }
     }
 
+    //Implementation for resetting password
     @Override
     public void resetPassword(String newPassword, String confirmPassword) {
-       try{
-           String email = (String) session.getAttribute("resetPasswordEmail");
-           if(email == null){
-               throw new IllegalStateException("Email not found in the session. User verification failed.");
-           }
-           if(!newPassword.equals(confirmPassword)){
-               throw new IllegalArgumentException("Password do not match.");
-           }
-           Users user = usersRepository.findByEmail(email).orElseThrow(() ->
-                   new ResourceNotFoundException("User doesn't exist with this email: "+email));
-           user.setPassword(DigestUtils.md5DigestAsHex(newPassword.getBytes()));
-           usersRepository.save(user);
-           log.info("Password reset for user: {}",email);
-           session.removeAttribute("resetPasswordVerificationCode");
-           session.removeAttribute("resetPasswordEmail");
-       }catch (Exception e){
-           log.error("Error during password reset", e);
-           throw new RuntimeException("Internal Server Error: "+e.getMessage());
-       }
+        try {
+            String email = (String) session.getAttribute("resetPasswordEmail");
+            if (email == null) {
+                throw new IllegalStateException("Email not found in the session. User verification failed.");
+            }
+            if (!newPassword.equals(confirmPassword)) {
+                throw new IllegalArgumentException("Password do not match.");
+            }
+            Users user = usersRepository.findByEmail(email).orElseThrow(() ->
+                    new ResourceNotFoundException("User doesn't exist with this email: " + email));
+            user.setPassword(DigestUtils.md5DigestAsHex(newPassword.getBytes()));
+            usersRepository.save(user);
+            log.info("Password reset for user: {}", email);
+            session.removeAttribute("resetPasswordVerificationCode");
+            session.removeAttribute("resetPasswordEmail");
+        } catch (Exception e) {
+            log.error("Error during password reset", e);
+            throw new RuntimeException("Internal Server Error: " + e.getMessage());
+        }
     }
 
-    private int generateVerificationCode(){
+    //private method for generating random code
+    private int generateVerificationCode() {
         Random random = new Random();
         return random.nextInt(900000) + 100000;
     }
