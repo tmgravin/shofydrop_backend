4e46dd909f5cccb036a1c237ac73fcd851eaf561
Ram Kumar Gautam <ramgautam698@gmail.com>
2024-07-17 16:55:08 +0545

    ram's change

--- src/main/java/com/shofydrop/dto/ResponseDto.java
+++ src/main/java/com/shofydrop/dto/ResponseDto.java
@@ -8,4 +8,22 @@ public class ResponseDto {
     private HttpStatus status;
     private String message;
     private Object data;
+	public HttpStatus getStatus() {
+		return status;
+	}
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
 }
