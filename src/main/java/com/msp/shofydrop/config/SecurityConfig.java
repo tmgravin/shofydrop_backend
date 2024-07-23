b64b3d9f2fab494f6fed2a25ff9d4abe68f17bf8
Kshitiz <kshitizbabik.37@gmail.com>
2024-07-23 21:20:32 +0545

    Security Config Package

--- src/main/java/com/msp/shofydrop/config/SecurityConfig.java
+++ src/main/java/com/msp/shofydrop/config/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.msp.shofydrop.database;
+package com.msp.shofydrop.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
