47b8e563c70bb908da219c2e10cac136ad63a149
Kshitiz <kshitizbabik.37@gmail.com>
2024-07-23 16:01:14 +0545

    Application.Properties Update

--- src/main/java/com/msp/shofydrop/database/SecurityConfig.java
+++ src/main/java/com/msp/shofydrop/database/SecurityConfig.java
@@ -2,22 +2,22 @@ package com.msp.shofydrop.database;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests(authorizeRequests ->
-                authorizeRequests
-                    .anyRequest().permitAll() // Allow all requests without authentication
-            )
-            .csrf().disable(); // Disable CSRF for simplicity, not recommended for production
-        return http.build();
-    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//            .authorizeRequests(authorizeRequests ->
+//                authorizeRequests
+//                    .anyRequest().permitAll() // Allow all requests without authentication
+//            )
+//            .csrf().disable(); // Disable CSRF for simplicity, not recommended for production
+//        return http.build();
+//    }
 }
