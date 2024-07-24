97ab803c6e166f89f641a5ec363eb664c8f50d97
RANAS7 <deepakrana.dr537@gmail.com>
2024-07-24 16:58:33 +0545

    Vendorkyc and user details

--- src/main/java/com/msp/shofydrop/authentication/serviceImpl/VendorKycServiceImpl.java
+++ src/main/java/com/msp/shofydrop/authentication/serviceImpl/VendorKycServiceImpl.java
@@ -1,13 +1,11 @@
 package com.msp.shofydrop.authentication.serviceImpl;
 
-import com.msp.shofydrop.authentication.entity.UserDetails;
 import com.msp.shofydrop.authentication.entity.VendorKyc;
 import com.msp.shofydrop.authentication.repository.UserDetailsRepo;
 import com.msp.shofydrop.authentication.repository.VndorKycRepo;
 import com.msp.shofydrop.authentication.service.VendorKycService;
 import com.msp.shofydrop.utils.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -33,13 +31,6 @@ public class VendorKycServiceImpl implements VendorKycService {
     @Override
     public String saveKyc(VendorKyc kyc, MultipartFile documentImageFront, MultipartFile documentImageBack) throws Exception {
         try {
-            UserDetails userDetails = userDetailsRepo.get(kyc.getVendorId()).get(0);
-
-            if (userDetails == null) {
-                throw new ResourceNotFoundException("UserDetails not exist: " + kyc.getVendorId());
-            }
-            userDetails.setIsKycCompleted('Y');
-            userDetailsRepo.saveUserDetails(userDetails);
 //
 //            UserDetails userDetails = userDetailsRepo.get(kyc.getVendorId()).get(0);
 //            userDetails.setIsKycCompleted('Y');
