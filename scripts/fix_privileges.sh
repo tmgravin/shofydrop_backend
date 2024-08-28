db515f887d8270073e9d7601a1b356c0e89b54a0
Chyanba <98306492+tmgravin@users.noreply.github.com>
2024-08-28 15:17:15 +0545

    Update fix_privileges.sh

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* jar
-chmod +x /home/ec2-user/server/server_start.sh
-chmod +x /home/ec2-user/server/server_stop.sh
-chmod +x /home/ec2-user/server/install_java.sh
+chmod +x /home/ec2-user/server/target/* jar
+chmod +x /home/ec2-user/server/scripts/server_start.sh
+chmod +x /home/ec2-user/server/scripts/server_stop.sh
+chmod +x /home/ec2-user/server/scripts/install_java.sh
